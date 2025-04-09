f9485058efd2706c3f938013d069cd864f7a547d
Vinogradnij <Vlud5@mail.ru>
2025-04-10 00:49:57 +0300

    Реализованы загрузка файла на стороне клиента и сохранение файла на стороне сервера

--- lesta_web/tfidf_table/views.py
+++ lesta_web/tfidf_table/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from . import forms
+from .utils import save_file
+
 
 def index(request):
-    context = {'title': 'Анализ tfidf'}
+    context = {
+        'title': 'Анализ tfidf',
+        'form': forms.FileForm(),
+    }
     return render(request, 'tfidf_table/index.html', context=context)
 
 
 def result(request):
     context = {'title': 'Результат анализа'}
+    if request.method == 'POST':
+        form = forms.FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            save_file(request.FILES['file'])
     return render(request, 'tfidf_table/result.html', context=context)
