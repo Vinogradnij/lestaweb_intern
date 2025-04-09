6379ba4c0225337d5eec7278db46dd9e8bc2d29a
Vinogradnij <Vlud5@mail.ru>
2025-04-10 00:47:13 +0300

    Добавлена метка app_name для использования в шаблонах

--- lesta_web/tfidf_table/urls.py
+++ lesta_web/tfidf_table/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tfidf_table'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('result/', views.result, name='result'),
