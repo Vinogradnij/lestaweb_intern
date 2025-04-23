26f69889140510397af80b07501d1573ba8a57c5
Vinogradnij <Vlud5@mail.ru>
2025-04-23 23:11:37 +0300

    Реализована форма, поддерживающая загрузку более одного файла

--- lesta_web/tfidf_table/forms.py
+++ lesta_web/tfidf_table/forms.py
@@ -1,5 +1,23 @@
 from django import forms
 
 
-class FileForm(forms.Form):
-    file = forms.FileField(label='Файл')
+class _MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class _MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", _MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+class UploadFilesForm(forms.Form):
+    file_field = _MultipleFileField(label='Файлы')
